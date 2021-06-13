5fd08989dbff910353f530265998620599523e35
Genius <Genius@DESKTOP-53Q4II1>
2021-06-13 16:35:48 +0200

    Add project files.

---
+++ Day5Lab II/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+/// <summary>
+/// This is lab5 on Events using Event Handler ::::
+/// if exam is started the event would fire ::::
+/// </summary>
+namespace Day5Lab_II
+{
+    enum ExamMode
+    {
+        start, end
+    }
+    class myEventArgs
+    {
+        public DateTime dt { get; set; }
+        public myEventArgs(DateTime dt)
+        {
+            this.dt = dt;
+        }
+    }
+    class Exam
+    {
+        public int id { get; set; }
+        public string subject { get; set; }
+        public int duration { get; set; }
+        public ExamMode mode;
+        public ExamMode examMode
+        {
+            get { return mode; }
+            set
+            {
+                mode = value;
+                if (value == ExamMode.start)
+                {
+                    if (myEvent != null)
+                        myEvent(this, new myEventArgs(DateTime.Now));
+                }
+
+            }
+        }
+
+        public Exam(int id,  string subject, int duration , ExamMode mode)
+        {
+            this.id = id;
+            this.subject = subject;
+            this.duration = duration;
+            this.mode = mode;
+
+            Student s = new Student();
+            myEvent += s.print;
+        }
+
+
+        public event EventHandler<myEventArgs> myEvent;
+    }
+    class Student
+    {
+        public void print(object obj , myEventArgs e)
+        {
+            Exam ex = obj as Exam;
+            Console.WriteLine($"exam {ex.examMode}ed duration: {ex.duration} hours   examID {ex.id} Subject: {ex.subject}\t time's now {e.dt}");           
+        }
+    }
+
+    class Staff
+    {
+        public static void print(Object obj, myEventArgs e)
+        {
+            Exam ex = obj as Exam;
+            Console.WriteLine($"exam {ex.examMode}ed duration: {ex.duration} hours   examID: {ex.id} Subject: {ex.subject}\t time's now {e.dt}");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exam ex = new Exam(101, "Math", 3, ExamMode.start);
+
+            ex.myEvent +=  Staff.print;
+
+            //   ex.examMode = ExamMode.start;
+            Console.WriteLine("Enter Exam Mode : 0 | Started \t 1 | end ");
+            ex.examMode = (ExamMode)Enum.Parse(typeof(ExamMode), Console.ReadLine());
+        }
+    }
+}
---
+++ Day5Lab/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+/// <summary>
+/// This is lab5 on Events using My Own Delegate :::
+/// if exam is started the event would fire ::::
+/// </summary>
+namespace Day5Lab
+{
+    public class myEventArgs
+    {
+        public DateTime dt;
+        public myEventArgs(DateTime dt)
+        {
+            this.dt = dt;
+        }
+    }
+    public enum ExamMode
+    {
+        start, end
+    }
+
+    public delegate void mydel(Exam ex, myEventArgs e);
+    public class Exam
+    {
+        public int ExamId { get; set; }
+     
+        public int durationOfExam { get; set; }
+
+        public string subjectName { get; set; }
+        ExamMode examMode;
+        public ExamMode _ExamMode
+        {
+            get { return examMode; }
+            set
+            {
+                examMode = value;
+                if (ExamMode.start == value)
+                {
+                    if (myEvent != null)
+                        myEvent(this, new myEventArgs(DateTime.Now));
+                }
+            }
+        }
+        public Exam(int id, int duration, string subject, ExamMode examMode)
+        {
+            this.ExamId = id;
+            this.durationOfExam = duration;
+            this.subjectName = subject;
+            this.examMode = examMode;
+
+            Student s = new Student();
+            Staff f = new Staff();
+            myEvent += s.print;
+            myEvent += f.print;
+        }
+        public event mydel myEvent;
+
+    }
+    public class Student
+    {
+        public void print(Exam ex, myEventArgs e)
+        {
+            Console.WriteLine($"examID :{ex.ExamId}  subject :{ex.subjectName}  exam mode: { ex._ExamMode}   exam duration :{ ex.durationOfExam} ");
+            Console.WriteLine($"time's now  {e.dt} ");
+        }
+    }
+    public class Staff
+    {
+        public void print(Exam ex, myEventArgs e)
+        {
+            Console.WriteLine($" examID :{ex.ExamId}  subject :{ex.subjectName}  exam mode: { ex._ExamMode}   exam duration :{ ex.durationOfExam} ");
+            Console.WriteLine($"time's now  {e.dt}");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exam ex = new Exam(1, 50, "OOP", ExamMode.start);
+
+            Console.WriteLine("Enter exam mode :  0 | start \t 1 | end");
+
+            string examModeValue = Console.ReadLine();
+
+            ex._ExamMode = (ExamMode)Enum.Parse(typeof(ExamMode), examModeValue);
+
+        }
+    }
+}
